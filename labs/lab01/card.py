79750a14624772d10a52c5af197a642becc19889
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-29 16:27:32 -0700

    Lab 01
    final state of lab 01

--- labs/lab01/card.py
+++ labs/lab01/card.py
@@ -44,30 +44,5 @@ class Card:
             print("That's not a valid number")
 
 
-class Deck:
-    def __init__(self):
-        self._cards = []
-        self.populate()
-
-    def populate(self):
-        suits = ["hearts", "clubs", "diamonds", "spades"]
-        numbers = [str(n) for n in range(2, 11)] + ["J", "Q", "K", "A"]
-        self._cards = [Card(s, n) for s in suits for n in numbers]
-
-    def shuffle(self):
-        random.shuffle(self._cards)
-
-    def deal(self, no_of_cards):
-        dealt_cards = []
-        for i in range(no_of_cards):
-            dealt_card = self._cards.pop(0)
-            dealt_cards.append(dealt_card)
-        return dealt_cards
-
-    def __repr__(self):
-        cards_in_deck = len(self._cards)
-        return "Deck of " + str(cards_in_deck) + " cards"
-
-
-deck = Deck()
-print(deck)
+card = Card()
+print(card)
